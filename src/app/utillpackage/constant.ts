4a8cb28b34c3ff2cda17262ce0c26b1686e2ea36
Avinash Kumar <avinashkumar4656@gmail.com>
2020-04-21 21:50:58 +0530

    constant

--- src/app/utillpackage/constant.ts
+++ src/app/utillpackage/constant.ts
@@ -1,7 +1,7 @@
 export class MyConstants {
-    static BASEURL = "http://localhost:8081/api/";
-    static serverURL = "http://localhost:8081";
+    // static BASEURL = "http://localhost:8081/api/";
+    // static serverURL = "http://localhost:8081";
 
-   // static BASEURL = "http://3.21.83.36:8081/api/";
-    //static serverURL = "http://3.21.83.36:8081";
+   static BASEURL = "http://3.21.83.36:8081/api/";
+    static serverURL = "http://3.21.83.36:8081";
 }
