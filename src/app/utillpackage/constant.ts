eee73cad4afe36dea483098aec1222cd18dd3574
Avinash kumar <avinashkumar4656@gmail.com>
2020-05-22 15:28:41 +0530

    final build

--- src/app/utillpackage/constant.ts
+++ src/app/utillpackage/constant.ts
@@ -1,6 +1,6 @@
 export class MyConstants {
     // static BASEURL = "http://localhost:8081/api/";
     // static serverURL = "http://localhost:8081";
-    static BASEURL = "http://3.21.83.36:8081/api/";
-    static serverURL = "http://3.21.83.36:8081";
+    static BASEURL = "https://app.getithomenow.com:8081/api/";
+    static serverURL = "https://app.getithomenow.com:8081/";
 }
